06998c6e6a3d9bc232e7c62dff282f35e9fce8e0
userA <nanthawat.s.m@hotmail.com>
2017-04-24 14:39:56 +0700

    UserA edit programA

--- programA.c
+++ programA.c
@@ -2,19 +2,12 @@
 #include <string.h>
 
 void func1(){
-<<<<<<< HEAD
-	printf("User A : message from func1 , I just change this program lololo from user A");
-=======
 	printf("User A : message from func1 , I just change this program lololo pqow");
->>>>>>> bd6046bec8fed1eeddb0606c6bb1d37e3c191e73
+	printf("User A : message to User C --> hellooo");
 }
 
 void main()
 { 
-<<<<<<< HEAD
 	printf("programA, I change here \n");
-=======
-	printf("programA laks\n");
->>>>>>> bd6046bec8fed1eeddb0606c6bb1d37e3c191e73
 	func1();
 }

5efbc3f464ba38815436e638c31ecfce156840f7
boatboat <boat.github@gmail.com>
2017-04-24 14:47:50 +0700

    by B

--- programA.c
+++ programA.c
@@ -3,12 +3,12 @@
 
 void func1(){
 
-	printf("User A : message from func1 , I just change this program lololo pqow");
+	printf("User A : message from func1 , I just change this program lololo pqow,C edit this");
 	printf("User A : message to User C --> hellooo");
 }
 
 void main()
 { 
-	printf("programA, I change here \n");
+	printf("programA, I change here ,C edit here\n");
 	func1();
 }

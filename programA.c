ed7980be0d6868574005f92f6d844d656e6e23e2
userA <nanthawat.s.m@hotmail.com>
2017-04-24 14:51:12 +0700

    edit programA

--- programA.c
+++ programA.c
@@ -4,11 +4,11 @@
 void func1(){
 
 	printf("User A : message from func1 , I just change this program lololo pqow");
-	printf("User A : message to User C --> hellooo");
+	printf("User A : message to User C --> hellooooo");
 }
 
 void main()
 { 
-	printf("programA, I change here \n");
+	printf("programA, I change here, change heree again... \n");
 	func1();
 }
